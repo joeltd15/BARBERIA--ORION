7943a9f60004fbf0dfe2bb6dc2f6f94eb45a78af
joeltd15 <joeltuiran15@gmail.com>
2024-03-12 16:16:54 -0500

    olvidar contraseña

--- BARBERIA-ORION/assets/js/login.js
+++ BARBERIA-ORION/assets/js/login.js
@@ -8,4 +8,26 @@ registerBtn.addEventListener('click', () => {
 
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
-});
+})
+
+function validateSignUp() {
+    const fullName = document.getElementById('fullName').value.trim();
+    const phoneNumber = document.getElementById('phoneNumber').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+    const confirmPassword = document.getElementById('confirmPassword').value.trim();
+
+    if (fullName === '' || phoneNumber === '' || email === '' || password === '' || confirmPassword === '') {
+        alert('Por favor complete todos los campos.');
+        return false; // Evitar el envío del formulario
+    }
+
+    if (password !== confirmPassword) {
+        alert('Las contraseñas no coinciden.');
+        return false; // Evitar el envío del formulario
+    }
+
+    // Puedes agregar más validaciones según tus necesidades aquí
+
+    return true; // Permitir el envío del formulario
+}
