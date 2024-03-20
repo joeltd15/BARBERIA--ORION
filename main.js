ca5c5929a1836b23170e4c8e33453974755d3f14
M1GU3L10 <123miguelfps@gmail.com>
2024-03-19 20:42:52 -0500

    BR remasterizado

--- main.js
+++ main.js
@@ -1,21 +1,36 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+const port = 2006;
 
-app.use(express.static('BARBERIA-ORION'))
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+// Servir contenido estático
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/servicios', (req, res) => {
+  res.render('servicios');
+});
+
 
 app.get('/productos', (req, res) => {
-  res.sendFile(__dirname+'/BARBERIA-ORION/Productos.html')
-})
+  res.render('Productos');
+});
 
 app.get('/login', (req, res) => {
-    res.sendFile(__dirname+'/BARBERIA-ORION/login.html')
-})
+  res.render('login');
+});
 
 app.get('*', (req, res) => {
-    res.sendFile(__dirname+'/BARBERIA-ORION/404.html')
-})
+  res.render('404');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`La aplicación de ejemplo está escuchando en el puerto ${port}`);
+});
